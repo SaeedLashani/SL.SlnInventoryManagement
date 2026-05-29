840e15498a3a104ded68dfd8133f183bc755c4b1
Saeed Lashani <saeed.lashani@handeln.de>
2026-05-29 14:55:05 +0200

    Add Azure & CI/CD

--- API/Program.cs
+++ API/Program.cs
@@ -49,11 +49,11 @@ builder.Services.AddJwtAuthentication(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
